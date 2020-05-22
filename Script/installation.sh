e48c0f8998577687f593dfc139f1aacf01ed3598
JSidat <JSidat@qa.com>
2020-05-22 19:46:00 +0000

    installation.sh correction

--- Script/installation.sh
+++ Script/installation.sh
@@ -17,4 +17,4 @@ source /var/lib/jenkins/workspace/flask_project/venv/bin/activate
 pip3 install -r requirements.txt
  
  
-gunicorn --bind=0.0.0.0:5000 app:app
+gunicorn --bind=0.0.0.0:5000 application:app
