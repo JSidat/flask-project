e7fe65b47a2efc60515cbb62a2a56265bc718de4
JSidat <JSidat@qa.com>
2020-05-23 15:45:58 +0000

    installation.sh change

--- Script/installation.sh
+++ Script/installation.sh
@@ -9,8 +9,6 @@ sudo apt install python3-pip -y
 sudo apt install python3-venv -y
  
 python3 -m venv venv
-
-source ~/.bashrc
  
 source /var/lib/jenkins/workspace/flask_project/venv/bin/activate
  
