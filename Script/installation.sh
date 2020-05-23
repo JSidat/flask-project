737f636a4bc11388976d460bd937defe7daaca79
JSidat <JSidat@qa.com>
2020-05-23 16:36:53 +0000

    installation.sh changes

--- Script/installation.sh
+++ Script/installation.sh
@@ -10,8 +10,10 @@ sudo apt install python3-venv -y
  
 python3 -m venv venv
  
-source /var/lib/jenkins/workspace/flask_project/venv/bin/activate
+cd /var/lib/jenkins/workspace/flask_project/venv/bin/activate
  
 pip3 install -r requirements.txt
+
+source ~/.bashrc
  
 gunicorn --bind=0.0.0.0:5000 application:app
