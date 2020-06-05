6d839d5dce7b0810ddf20cba8cea183c3a86a210
JSidat <JSidat@qa.com>
2020-06-05 14:46:15 +0000

    typo

--- application/__init__.py
+++ application/__init__.py
@@ -5,9 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']=getenv('FLASK_PROJECT_DB_URI')
+app.config['SQLALCHEMY_DATABASE_URI']=getenv('FLASKPROJECTDBURI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-app.config['SECRET_KEY']=getenv('SECRET_KEY')
+app.config['SECRET_KEY']=getenv('SECRETKEY')
 db = SQLAlchemy(app)
 
 from application import routes
